44ae3bb0292dbdba3aad67bc1026265b5437c9bd
jpcoutinho <jp.coutiinho@gmail.com>
2020-03-19 17:36:34 -0300

    Updates

---
+++ precache-manifest.d988e565f3596b6bc1d30aeafa1c1a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878fb260d8a3de11e131439ff92ee86e",
+    "url": "/challenge-chart-plot/index.html"
+  },
+  {
+    "revision": "43e10e1c58650ce7a830",
+    "url": "/challenge-chart-plot/static/css/main.1d8ecdd7.chunk.css"
+  },
+  {
+    "revision": "3cfadb5383dd9e7bdfaa",
+    "url": "/challenge-chart-plot/static/js/2.933675f7.chunk.js"
+  },
+  {
+    "revision": "adee41ef7752baba7fc509c48b8970c7",
+    "url": "/challenge-chart-plot/static/js/2.933675f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e10e1c58650ce7a830",
+    "url": "/challenge-chart-plot/static/js/main.6624477f.chunk.js"
+  },
+  {
+    "revision": "19360a93099d330921e1",
+    "url": "/challenge-chart-plot/static/js/runtime-main.8ee14943.js"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/challenge-chart-plot/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  }
+]);
